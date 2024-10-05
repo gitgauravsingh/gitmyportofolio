12165411ffd848c88726f05c97d5a88a75d486e5
gitgauravsingh <gauravsurvanshi2002@gmail.com>
2024-10-05 15:55:02 +0530

    message

---
+++ html/js/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const linebar = document.getElementById("linebar");
+  const opennav = document.getElementById("opennav");
+  linebar.addEventListener("click", function () {
+    this.classList.toggle("active");
+    if (opennav.style.width === "250px") {
+      opennav.style.width = "0";
+    } else {
+      opennav.style.width = "250px";
+    }
+  });
+
+  $(".nav ul li:first").addClass("active");
+  $(".nav ul li").click(function () {
+    $(".nav ul li").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $(".imagesection").hide();
+  $(".imagesection:first").show();
+  $(".tabdata ul li:first").addClass("active");
+  $(".tabdata ul li").click(function () {
+    $(".tabdata ul li").removeClass("active");
+    $(this).addClass("active");
+    $(".imagesection").hide();
+    let ShowTab = $(this).find("a").attr("href");
+    $(ShowTab).fadeIn();
+    return false;
+  });
+});
